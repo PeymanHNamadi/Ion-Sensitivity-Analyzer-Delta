7ec5f553eb21f7a3d94102e31ae19d7cb73aaf75
PeymanHNamadi <99760532+PeymanHNamadi@users.noreply.github.com>
2024-06-25 13:40:27 -0700

    ipynb

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 # assume that the container already has activated the right environment
 # asumme current working directory is at the top level of this repo
-panel serve Sensitivity_IonStudy.py --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
+panel serve Sensitivity_IonStudy.ipynb --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
